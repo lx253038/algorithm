1d3bb1936a9ae4f1b0c5c71f059bfb2b034bf457
lixin1sj <lixin1sj@yonyou.com>
2018-11-12 18:13:15 +0800

    二分搜索树给定两个节点查询出最近的父节点

---
+++ src/Tree/LowestCommonAncestor.java
@@ -0,0 +1,46 @@
+package Tree;
+
+/**
+ * _______6______
+ * /              \
+ * ___2__          ___8__
+ * /      \        /      \
+ * 0       4       7       9
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * <p>
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ */
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        LowestCommonAncestor lc = new LowestCommonAncestor();
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(9);
+        root = lc.lowestCommonAncestor(root, p, q);
+        System.out.println(root.val);
+    }
+
+}

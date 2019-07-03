0fb69da2b4c0108dba1b3ae09954e4d56413acf5
lixin1sj <lixin1sj@yonyou.com>
2019-07-03 18:26:18 +0800

    CountDownLatch计时器

---
+++ src/Thread/CountDownLatchDemo.java
@@ -0,0 +1,29 @@
+package Thread;
+
+import java.util.concurrent.CountDownLatch;
+
+import Enum.SixKing;
+
+/**
+ * @author LX
+ * @date 2019-7-3 17:50
+ * 倒计时器
+ */
+//设置倒计时初始值，调用countDown()方法减1，使用await()阻塞线程等待倒计时为0方可执行
+public class CountDownLatchDemo {
+
+    private static CountDownLatch count = new CountDownLatch(6);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "号同学离开！");
+                count.countDown();
+            }, SixKing.findById(String.valueOf(i)).getName()).start();
+        }
+        count.await();
+        System.out.println(Thread.currentThread().getName() + "关闭教室门");
+    }
+
+}

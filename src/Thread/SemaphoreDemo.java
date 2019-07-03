26b1f6ad8847fcb2abea5148e2a3cf2315b6e75c
lixin1sj <lixin1sj@yonyou.com>
2019-07-03 20:41:37 +0800

    Semaphore信号量

---
+++ src/Thread/SemaphoreDemo.java
@@ -0,0 +1,38 @@
+package Thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author LX
+ * @date 2019-7-3 20:21
+ * Semaphore信号量 用来控制同时访问某个特定资源的操作数量，或者同时执行某个指定操作的数量
+ * 例：10辆车看成10个线程，抢占 3个车位（即Semaphore设置数量等于3）;
+ * 调用acquire()方法抢占资源，未抢到的则等待，release()释放资源
+ */
+
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 1; i <= 10; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "号抢到车位！");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName() + "号停三秒，释放车位！");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+
+
+            }, String.valueOf(i)).start();
+        }
+
+
+    }
+}

0efd12e8dcfff6123081576c70a0ccc3f6af9e77
lixin1sj <lixin1sj@yonyou.com>
2019-09-02 15:04:06 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        String user = new String(new char[]{'7', '8', '9', 'G'}, 1, 2);
         String user1 = new String(new int[]{51, 52, 53, 54}, 0, 2);
         System.out.println(user1.codePointAt(0));
         System.out.println(user1);

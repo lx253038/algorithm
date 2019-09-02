5f9b994e4379eac609ee331e589867c7a22514ba
lixin1sj <lixin1sj@yonyou.com>
2019-09-02 15:02:23 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String user = new String(new char[]{'7', '8', '9', 'G'}, 1, 2);
+        String user1 = new String(new int[]{51, 52, 53, 54}, 0, 2);
+        System.out.println(user1.codePointAt(0));
+        System.out.println(user1);
     }
 }

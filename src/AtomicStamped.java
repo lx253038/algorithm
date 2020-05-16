d4d74c1de79d235e468a811772503dbc5504cea8
lixin1sj <lixin1sj@yonyou.com>
2020-05-16 14:41:48 +0800

    带版本号的原子类操作  解决了ABA问题

--- src/AtomicStamped.java
+++ src/AtomicStamped.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 /**
  * @Author LX
  * @Date 2019/11/2 12:53
- * @Description TODO
+ * @Description 带版本号的原子类操作  解决了ABA问题
  */
 public class AtomicStamped {
 
@@ -16,14 +16,15 @@ public class AtomicStamped {
                 for (int j = 0; j < 100; j++) {
                     int stamp = reference.getStamp();
                     try {
-                        TimeUnit.MILLISECONDS.sleep(20);
+                        TimeUnit.MILLISECONDS.sleep(10);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     boolean b = reference.compareAndSet(100, 100, stamp, stamp + 1);
                     if (b) {
                         System.out.println(Thread.currentThread().getName() + ":" + "(是否修改成功：" + b + ",当前版本号：" + reference.getStamp() + ")");
-
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + ":" + "(修改失败：" + b + ",拿到的版本号：" + stamp + ",当前版本号：" + reference.getStamp() + ")");
                     }
                 }
 

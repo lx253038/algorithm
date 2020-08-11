981ddfee3ab812b7df09f92ea29c64336674bb4d
lixin1sj <lixin1sj@yonyou.com>
2020-08-11 19:13:10 +0800

    字符串反转

---
+++ src/String/LeetCode125.java
@@ -0,0 +1,43 @@
+package String;
+
+/**
+ * @Author LX
+ * @Date 2020/8/11 18:50
+ * @Description 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * 示例 1:
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 示例 2:
+ * 输入: "race a car"
+ * 输出: false
+ */
+public class LeetCode125 {
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode125().isPalindrome("race a car"));
+    }
+
+    public boolean isPalindrome(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        if (chars.length < 2) {
+            return true;
+        }
+        int l = 0, r = chars.length - 1;
+        while (l <= r) {
+            if (!Character.isLetterOrDigit(chars[l])) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(chars[r])) {
+                r--;
+                continue;
+            }
+            if (chars[l++] != chars[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/String/LeetCode344.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.Arrays;
+
+/**
+ * @Author LX
+ * @Date 2020/8/11 19:07
+ * @Description编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ */
+public class LeetCode344 {
+
+    public static void main(String[] args) {
+        new LeetCode344().reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
+    }
+
+    public void reverseString(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            char tmp = s[l];
+            s[l++] = s[r];
+            s[r--] = tmp;
+        }
+        System.out.println(Arrays.toString(s));
+    }
+}
